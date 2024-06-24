8d481a93db80fdce88691afe8934fbafad66739e
joevt <joevt@shaw.ca>
2024-06-24 02:21:35 -0700

    Fix printf compiler warning

--- ls-ecaps.c
+++ ls-ecaps.c
@@ -1692,7 +1692,7 @@ cap_ide(struct device *d, int where)
             base = get_conf_long(d, off + 8);
             base <<= 32;
             base |= PCI_IDE_SEL_ADDR_1_BASE_LOW(l) << 20;
-            printf("\t\t%sSelectiveIDE#%d RID#%d: Valid%c Base=%lx Limit=%lx\n",
+            printf("\t\t%sSelectiveIDE#%d RID#%d: Valid%c Base=%llx Limit=%llx\n",
               offstr(offs, off),
               i,
               j,
